78b07dbca666886d7da60c55ce5f6f9e27194ef4
Florian Chiraux <florian.chiraux@kpf-si.fr>
2022-05-06 15:20:20 +0200

    Add method to get a cross chain account for a symbol

--- src/main/java/fr/florianchrx/tradingAPI/repositories/AccountRepository.java
+++ src/main/java/fr/florianchrx/tradingAPI/repositories/AccountRepository.java
@@ -1,10 +1,13 @@
 package fr.florianchrx.tradingAPI.repositories;
 
 import fr.florianchrx.tradingAPI.model.Account;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * The repository to interact with accounts in DB.
  */
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    @Query(nativeQuery = true, value = "select id_account, address, symbol, amount from account_cross_chain where symbol = ?1")
+    Account findBySymbolId(long symbolId);
 }
